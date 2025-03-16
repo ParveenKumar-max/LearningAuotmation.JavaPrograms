f53d766ea9814bc33ff5ef4947a2814fff223655
ParveenKumar-max <Parveendogra2@gmail.com>
2025-03-16 21:01:32 +0530

    Java Basics - Print Triangle Classifier

---
+++ src/Ex_01_JavaBasics/Ex19_TriangleProgram.java
@@ -0,0 +1,37 @@
+package Ex_01_JavaBasics;
+
+import java.util.Scanner;
+
+public class Ex19_TriangleProgram {
+    public static void main(String[] args) {
+        // ✅ Triangle Classifier:
+        // Write a program that classifies a triangle based on its side lengths.
+        // Given three input values representing the lengths of the sides,
+        // determine if the triangle is equilateral (all sides are equal),
+        // s1 == s2 && s2 == s3 && s1 == s3 -- is equilateral
+        // isosceles (exactly two sides are equal),
+        // s1 == s2 && s1 == s3 -- is isosceles
+        // or scalene (no sides are equal)
+        // Use an if-else statement to classify the triangle.
+
+        // Given three input values representing the lengths of the sides,
+        Scanner scanner_s1 = new Scanner(System.in);
+        int value1 = scanner_s1.nextInt();
+        Scanner scanner_s2 = new Scanner(System.in);
+        int value2 = scanner_s2.nextInt();
+        Scanner scanner_s3 = new Scanner(System.in);
+        int value3 = scanner_s3.nextInt();
+
+        if (value1 == value2 && value2 == value3 && value1 == value3) {
+            System.out.println("It's equilateral Triangle");
+        }
+            else if(value1 == value2 || value2 == value3 || value1 == value3) {
+            System.out.println("It's is isosceles");
+        }
+            else {
+            System.out.println("No Sides are equal");
+
+        }
+
+        }
+    }
