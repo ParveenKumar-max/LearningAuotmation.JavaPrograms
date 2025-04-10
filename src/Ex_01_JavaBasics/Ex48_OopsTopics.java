9fb988f6810cfc0158cf1bae24e92d9b9a7b058c
ParveenKumar-max <Parveendogra2@gmail.com>
2025-04-10 12:06:07 +0530

    Java Basics - OOP's Concept --> Class & Object

---
+++ src/Ex_01_JavaBasics/Ex48_OopsTopics.java
@@ -0,0 +1,29 @@
+package Ex_01_JavaBasics;
+
+public class Ex48_OopsTopics {
+    public static void main(String[] args) {
+
+        // Attributes
+        String name;
+        int age;
+        String course;
+        String email;
+        int salary;
+        String gender;
+
+
+        // Behaviour
+  /*      void speak() {
+        }
+        void doAssignment () {
+        }
+        void doTask () {
+        }
+        void askQuestion () {
+        }
+        void learnCoding () {
+        }
+*/
+
+    }
+}
---
+++ src/Ex_01_JavaBasics/Ex49_OOPsClassObject.java
@@ -0,0 +1,32 @@
+package Ex_01_JavaBasics;
+
+public class Ex49_OOPsClassObject {
+    public static void main(String[] args) {
+
+        // @Students is a clas loading @value1 is reference variable @new Students is Object.
+        Students value1 = new Students();
+        value1.name = "Parveen";
+        value1.age = 30;
+        System.out.println(value1);
+        System.out.println(value1.name);
+        System.out.println(value1.age);
+
+
+        Students value2 = new Students();
+        value2.name = "Pintu Chaudhary";
+        value2.age = 31;
+        System.out.println(value2);
+
+
+}
+
+   static class Students {
+        String name;
+        int age;
+
+
+        public String toString(){
+            return "Name is:"+ name + ";" + "Age is: " + age;
+        }
+    }
+}
